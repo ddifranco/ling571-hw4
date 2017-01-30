b19cb587619553042c273fd20fa605242d58cc2b
Daniel DiFranco <dpdifran@patas.ling.washington.edu>
2017-01-29 21:23:42 -0800

    Adding timers to the two parsing programs

--- hw4_run.sh
+++ hw4_run.sh
@@ -4,8 +4,6 @@ TREEBANK='/opt/dropbox/16-17/571/hw4/data/parses.train'
 GOLD='/opt/dropbox/16-17/571/hw4/data/parses.gold'
 TESTS='/opt/dropbox/16-17/571/hw4/data/sentences.txt'
 PCFG='./out/hw4_trained.pcfg'
-#TESTS='./data/toy_sentences.txt'
-#PCFG='./data/toy.pcfg'
 BASEOUT='./out/parses_base.out'
 BEAMOUT='./out/parses_improved.out'
 TOOLS='/opt/dropbox/16-17/571/hw4/tools'
@@ -13,16 +11,16 @@ BASERES='./out/parses_base.eval'
 BEAMRES='./out/parses_improved.eval'
 
 #PCFG Estimation
-#./hw4_topcfg.py $TREEBANK $PCFG
+./hw4_topcfg.py $TREEBANK $PCFG
 
 #Baseline Parses 
-#./hw4_parser.sh $PCFG $TESTS $BASEOUT
+time ./hw4_parser.sh $PCFG $TESTS $BASEOUT
 
 #Baseline Evaluation 
-#$TOOLS/evalb -p $TOOLS/COLLINS.prm $GOLD $BASEOUT > $BASERES
+$TOOLS/evalb -p $TOOLS/COLLINS.prm $GOLD $BASEOUT > $BASERES
 
 #Beam Parses (optionally specify n as last argument; defaults to n=x)
-#./hw4_parser_improved.sh $PCFG $TESTS $BEAMOUT
+time ./hw4_parser_improved.sh $PCFG $TESTS $BEAMOUT
 
 #Beam Evaluation
 $TOOLS/evalb -p $TOOLS/COLLINS.prm $GOLD $BEAMOUT > $BEAMRES
