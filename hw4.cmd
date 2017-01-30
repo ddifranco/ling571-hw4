f7bdb08cd6ecda74d7731ce58f480206add8f429
Daniel DiFranco <dpdifran@patas.ling.washington.edu>
2017-01-29 21:21:59 -0800

    added condor files; deleted outfiles

---
+++ hw4.cmd
@@ -0,0 +1,10 @@
+universe = vanilla
+executable = /bin/bash
+getenv = true
+input = dummy.in
+output = myoutput.out
+error = myerror.err
+log = log
+arguments = "hw4_run.sh"
+transfer_executable=false
+queue
